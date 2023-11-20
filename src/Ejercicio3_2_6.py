14248bd10211dbdf28738c6cd2021a4a18dbfe5e
JoseAntonio <jdiabus511@g.educaand.es>
2023-11-20 19:05:30 +0100

    Diccionarios

---
+++ src/Ejercicio3_2_6.py
@@ -0,0 +1,10 @@
+"""
+Escribir un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) 
+que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+"""
+def 
+
+def main():
+    info={}
+if __name__=="__main__":
+    main()
